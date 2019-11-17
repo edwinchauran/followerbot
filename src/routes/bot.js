1a58d0a71ebfe547bafb97d5f52dff469866ea5e
edwin <thedwinvisual@gmail.com>
2019-11-16 20:45:40 -0400

    Commit de prueba de git modificado taskboard

--- src/routes/bot.js
+++ src/routes/bot.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Account = require('../models/Account');
 
-// router.post('/bot', async (req, res) => {
-//     const cod = req.body;
-//     const dato = await Account.find({id:cod})
-//     res.render('followerbot/bot', {id});
-// })
+router.post('/bot', async (req, res) => {
+    const cod = req.body;
+    const dato = await Account.find({id:cod})
+    res.render('followerbot/bot', {id});
+})
 
 router.get('/bot', async (req, res) => {
     if(!req.query) {
@@ -15,11 +15,11 @@ router.get('/bot', async (req, res) => {
     else {
         const cod = req.query.name;
         // res.send(cod);
-        const username = await Account.find({accountToFollow: {username:cod}});
-        const accountToFollow = await Account.find({$filter : { accountToFollow: {$eq: cod}}});
+        // const username = await Account.find({accountToFollow: {username:cod}});
+        // const accountToFollow = await Account.find({$filter : { accountToFollow: {$eq: cod}}});
         const datos = await Account.find({username:cod});
-        // res.send({datos});
         res.render('followerbot/bot', {datos});
     }
 })
 module.exports = router;
+//
